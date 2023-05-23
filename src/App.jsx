0717ed996c629ecbe9dca445de1d684cd88bc0d1
MaylenHaag <maylenhaag23@gmail.com>
2023-05-23 20:23:17 -0300

    clase 5

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <ItemListContainer greeting={'¡Te estabamos esperando!'}/>
+      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada ', quantity)} />
+    </div>
+  );
+}
+
+export default App;
